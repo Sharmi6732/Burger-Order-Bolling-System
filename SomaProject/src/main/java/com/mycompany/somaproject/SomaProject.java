10e23082f55515914f816985e76ca5d5926a7cea
Somashree Barman <89794489+Sharmi6732@users.noreply.github.com>
2023-09-06 02:14:20 +0530

    Add files via upload

---
+++ SomaProject/src/main/java/com/mycompany/somaproject/SomaProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.somaproject;
+
+/**
+ *
+ * @author Somashree Barman
+ */
+public class SomaProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
